4f50f38d7d4ff5fd502d7ba3caf0f05aa45982d6
Qiaosu <tracy.xin.li@gmail.com>
2012-11-26 17:23:59 +0800

    -log

--- jsCodefragment/log.js
+++ jsCodefragment/log.js
@@ -2,10 +2,36 @@
  * Name: {log}
  * Tags: {console.log, utility}
  * Description: console.log加强版，增加了浏览器支持判断。
- * Recommend: 樵苏
+ * Recommend: 天孝
+ * Usage:
+ * $NAMESPACE.log.enable = true; //开发环境设true，线上设false；
+ * $NAMESPACE.log('xxx')
+ * $NAMESPACE.log.info('xxx')
+ * $NAMESPACE.log.warn('xx', 'xxx')
+ * $NAMESPACE.log.error('xx', 'xx', 'xxx')
  */
 $NAMESPACE['log'] = function(){
-    window.console && 
-    console.log && 
-    console.log.apply(console, arguments);
+    var types = "log|info|warn|error".split("|");
+    var _trace = function(){
+        var args = arguments;
+        if(_trace.enable && args.length){
+            var type = "log", cs = console;
+            if(args.length >=2 && new RegExp(types.join("|"), "i").test(args[args.length - 1])){
+                type = (args = [].slice.call(args)).pop();
+            }
+            if(cs && cs[type] && typeof(cs[type]) == "function"){
+                cs[type].apply(cs, args);
+            }
+        }
+    };
+
+    while(type = types.pop()){
+        _trace[type] = (function(type){
+            return function(){
+                this.apply(this, [].slice.call(arguments).concat(type));
+            };
+        }(type));
+    }
+
+    return _trace;
 };
