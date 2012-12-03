06b740363365793507efabd5826b228795fc057b
Qiaosu <tracy.xin.li@gmail.com>
2012-12-03 10:10:05 +0800

    +log update

--- jsCodefragment/log.js
+++ jsCodefragment/log.js
@@ -10,28 +10,30 @@
  * $NAMESPACE.log.warn('xx', 'xxx')
  * $NAMESPACE.log.error('xx', 'xx', 'xxx')
  */
-$NAMESPACE['log'] = function(){
-    var types = "log|info|warn|error".split("|");
-    var _trace = function(){
+$NAMESPACE['log'] = function() {
+    var types = "log|info|warn|error".split("|"),
+        i = 0,
+        type;
+    var _trace = function() {
         var args = arguments;
-        if(_trace.enable && args.length){
-            var type = "log", cs = console;
-            if(args.length >=2 && new RegExp(types.join("|"), "i").test(args[args.length - 1])){
+        if(_trace.enable && args.length) {
+            var type = "log",
+                cs = console;
+            if(args.length >= 2 && new RegExp(types.join("|"), "i").test(args[args.length - 1])) {
                 type = (args = [].slice.call(args)).pop();
             }
-            if(cs && cs[type] && typeof(cs[type]) == "function"){
+            if(cs && cs[type] && typeof(cs[type]) == "function") {
                 cs[type].apply(cs, args);
             }
         }
     };
-
-    while(type = types.pop()){
-        _trace[type] = (function(type){
-            return function(){
+    while(type = types[i]) {
+        _trace[type] = (function(type) {
+            return function() {
                 this.apply(this, [].slice.call(arguments).concat(type));
             };
         }(type));
+        i++;
     }
-
     return _trace;
 };
